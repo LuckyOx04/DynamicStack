533da707a5a2b23f868408df75de0a6a87bd0c92
LuckyOx04 <ivailon27@gmail.com>
2022-03-17 21:52:26 +0200

    Made the example data a palindrome.

--- Program.cpp
+++ Program.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 int main(){
 	DynamicStack<string>* stack = new DynamicStack<string>();
-	stack -> push("O");
-	stack -> push("L");
-	stack -> push("L");
+	stack -> push("R");
+	stack -> push("A");
+	stack -> push("C");
 	stack -> push("E");
-	stack -> push("H");
+	stack -> push("C");
+	stack -> push("A");
+	stack -> push("R");
 
 	int count = stack -> getCount();
 
